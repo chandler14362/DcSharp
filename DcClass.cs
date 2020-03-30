61650b89968bcf2028d1d6e6ecd3db81b184f6d9
Chandler Stowell <chandler14362@gmail.com>
2020-03-30 15:48:45 -0400

    flesh out DcClass with documentation

--- DcClass.cs
+++ DcClass.cs
@@ -11,16 +11,34 @@ namespace DcSharp
         
         public static bool SortInheritanceByFile = true;
         
+        /// <value>
+        /// The name of the <c>DcClass</c>
+        /// </value>
         public string Name { get; private set; }
         
-        public DcFile File { get; private set; }
+        /// <value>
+        /// The <c>DcFile</c> defining the <c>DcClass</c>
+        /// </value>
+        public DcFile? File { get; private set; }
         
+        /// <value>
+        /// A flag indicating if the <c>DcClass</c> is a struct or not.
+        /// </value>
         public bool IsStruct { get; set; }
         
+        /// <value>
+        /// A flag indicating if the <c>DcClass</c> is bogus or not.
+        /// </value>
         public bool Bogus { get; set; }
         
+        /// <value>
+        /// The <c>DcClass</c> number.
+        /// </value>
         public int Number { get; set; }
 
+        /// <value>
+        /// The <c>DcClass</c> constructor.
+        /// </value>
         public DcField? Constructor { get; private set; }
         
         private List<DcClass> _parents;
@@ -29,23 +47,14 @@ namespace DcSharp
         private Dictionary<string, DcField> _fieldsByName;
         private Dictionary<int, DcField> _fieldsByIndex;
 
+        /// <value>
+        /// The number of fields defined inside the <c>DcClass</c>
+        /// </value>
         public int NumFields => _fields.Count;
 
-        public DcClass(DcFile dcFile, string name, bool isStruct, bool isBogus)
-        {
-            File = dcFile;
-            Name = name;
-            IsStruct = isStruct;
-            Bogus = isBogus;
-            Number = -1;
-
-            _parents = new List<DcClass>();
-            _fields = new List<DcField>();
-            _inheritedFields = new List<DcField>();
-            _fieldsByName = new Dictionary<string, DcField>();
-            _fieldsByIndex = new Dictionary<int, DcField>();
-        }
-        
+        /// <value>
+        /// The number of inherited fields inside the DcClass
+        /// </value>
         public int NumInheritedFields
         {
             get
@@ -67,6 +76,26 @@ namespace DcSharp
             }
         }
         
+        public DcClass(DcFile? dcFile, string name, bool isStruct, bool isBogus)
+        {
+            File = dcFile;
+            Name = name;
+            IsStruct = isStruct;
+            Bogus = isBogus;
+            Number = -1;
+
+            _parents = new List<DcClass>();
+            _fields = new List<DcField>();
+            _inheritedFields = new List<DcField>();
+            _fieldsByName = new Dictionary<string, DcField>();
+            _fieldsByIndex = new Dictionary<int, DcField>();
+        }
+
+        /// <summary>
+        /// Adds a field to the <c>DcClass</c>
+        /// </summary>
+        /// <param name="field">The field to add</param>
+        /// <returns>True on success, false on error</returns>
         public bool AddField(DcField field)
         {
             if (field.Class != null && field.Class != this)
@@ -110,10 +139,14 @@ namespace DcSharp
             return true;
         }
 
-        internal void AddParent(DcClass parent)
+        /// <summary>
+        /// Adds a parent <c>DcClass</c> to the <c>DcClass</c>
+        /// </summary>
+        /// <param name="parent">The parent</param>
+        public void AddParent(DcClass parent)
         {
             _parents.Add(parent);
-            File.MarkInheritedFieldsStale();
+            File?.MarkInheritedFieldsStale();
         }
 
         internal void RebuildInheritedFields()
@@ -154,13 +187,13 @@ namespace DcSharp
                 }
             }
             
-            _inheritedFields.Sort((a, b) => a.Number < b.Number ? -1 : 1);
+            _inheritedFields.Sort((a, b) => a.Number > b.Number ? 1 : -1);
             
         }
 
         internal void ClearInheritedFields()
         {
-            _inheritedFields.Clear(); // TODO: Check integrity of method
+            _inheritedFields.Clear();
         }
 
         private void ShadowInheritedField(string name)
@@ -175,6 +208,11 @@ namespace DcSharp
             }
         }
 
+        /// <summary>
+        /// Gets the nth field defined inside the <c>DcClass</c>
+        /// </summary>
+        /// <param name="n">The field index</param>
+        /// <returns>The field or null</returns>
         public DcField? GetField(int n)
         {
             if (n < 0 || n >= _fields.Count)
@@ -183,12 +221,23 @@ namespace DcSharp
             return _fields[n];
         }
 
+        /// <summary>
+        /// Tries to get the nth field defined inside the <c>DcClass</c>
+        /// </summary>
+        /// <param name="n">THe field index</param>
+        /// <param name="field">The field</param>
+        /// <returns>True on success, false on error</returns>
         public bool TryGetField(int n, out DcField field)
         {
             field = GetField(n);
             return field != null;
         }
 
+        /// <summary>
+        /// Gets a field by name within the <c>DcClass</c>
+        /// </summary>
+        /// <param name="name">The name of the field</param>
+        /// <returns>The field or null</returns>
         public DcField? GetFieldByName(string name)
         {
             if (_fieldsByName.TryGetValue(name, out var field))
@@ -201,12 +250,23 @@ namespace DcSharp
             return null;
         }
 
+        /// <summary>
+        /// Tries to get a field by name within the <c>DcClass</c>
+        /// </summary>
+        /// <param name="name">The name of the field</param>
+        /// <param name="field">The field</param>
+        /// <returns>True on success, false on error</returns>
         public bool TryGetFieldByName(string name, out DcField field)
         {
             field = GetFieldByName(name);
             return field != null;
         }
 
+        /// <summary>
+        /// Gets a field by global index inside the <c>DcClass</c>
+        /// </summary>
+        /// <param name="index">The index of the field</param>
+        /// <returns>The field or null</returns>
         public DcField? GetFieldByIndex(int index)
         {
             if (_fieldsByIndex.TryGetValue(index, out var field))
@@ -224,12 +284,23 @@ namespace DcSharp
             return null;
         }
 
+        /// <summary>
+        /// Tries to get a field with a given index inside the <c>DcClass</c>
+        /// </summary>
+        /// <param name="index">The field index</param>
+        /// <param name="field">The field</param>
+        /// <returns>True on success, false on error</returns>
         public bool TryGetFieldByIndex(int index, out DcField field)
         {
             field = GetFieldByIndex(index);
             return field != null;
         }
 
+        /// <summary>
+        /// Gets the nth inherited field.
+        /// </summary>
+        /// <param name="n">The inherited field index</param>
+        /// <returns>The inherited field or null</returns>
         public DcField? GetInheritedField(int n)
         {
             if (MultipleInheritance && VirtualInheritance)
@@ -253,10 +324,29 @@ namespace DcSharp
             }
         }
 
+        /// <summary>
+        /// Tries to get the nth inherited field.
+        /// </summary>
+        /// <param name="n">The inherited field index</param>
+        /// <param name="field"></param>
+        /// <returns>True on success, false on error</returns>
         public bool TryGetInheritedField(int n, out DcField field)
         {
             field = GetInheritedField(n);
             return field != null;
+        }
+
+        /// <summary>
+        /// Gets the index of an inherited field
+        /// </summary>
+        /// <param name="field">The field</param>
+        /// <returns>The field index or -1</returns>
+        public int GetInheritedFieldIndex(DcField field)
+        {
+            if (!_inheritedFields.Contains(field))
+                return -1;
+            
+            return _inheritedFields.IndexOf(field);
         }
 
         public override string ToString()
